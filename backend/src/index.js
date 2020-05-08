2133d5f6dc5e1c01940c9495559cd13b7da97dc5
Luan Dantas <luan_h.s.d@hotmail.com>
2020-05-08 13:08:10 -0300

    dotenv add

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+require('dotenv/config');
 
 const app = express()
 
 mongoose.connect(
-    'mongodb+srv://Omnistack:Omnistack@omnistacks-iqkby.mongodb.net/reactbox?retryWrites=true&w=majority', {
+    process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
@@ -18,4 +19,6 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(require('./routes'))
 
-app.listen(3001)
+app.listen(process.env.PORT, () => {
+    console.info(`Running server on port ${process.env.PORT}`);
+});
